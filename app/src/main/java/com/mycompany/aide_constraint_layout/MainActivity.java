fa75310825911fd9478056fa7d9893e7917da188
MATSUDA Takashi <t.matsuda.1000@gmail.com>
2019-09-16 12:53:00 +0900

    project template

---
+++ app/src/main/java/com/mycompany/aide_constraint_layout/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mycompany.aide_constraint_layout;
+
+import android.app.*;
+import android.os.*;
+
+public class MainActivity extends Activity 
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
